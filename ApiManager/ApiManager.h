609871e1a32f521626c8d626670036cd91724d9e
mohnish <vmohnish@thisisswitch.com>
2019-02-13 17:21:00 +0530

    Initial Commit

---
+++ ApiManager/ApiManager.h
@@ -0,0 +1,19 @@
+//
+//  ApiManager.h
+//  ApiManager
+//
+//  Created by Vardhan on 13/02/19.
+//  Copyright © 2019 switchSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApiManager.
+FOUNDATION_EXPORT double ApiManagerVersionNumber;
+
+//! Project version string for ApiManager.
+FOUNDATION_EXPORT const unsigned char ApiManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApiManager/PublicHeader.h>
+
+
